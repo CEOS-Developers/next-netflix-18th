3c048ebb043308d358e207eefafcd94b7b7e50a4
leejin_rho <tbdpapdl@gmail.com>
2023-11-07 17:14:01 +0900

    chore: init setting

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/binary_ro/Documents/ceos/next-netflix-18th/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
