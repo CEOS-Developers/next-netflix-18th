34d195d224efc8533fa5d0c3f00e867a27184168
inyoung <rmdnps10@gmail.com>
2023-11-09 15:32:38 +0900

    Feat: global.css html 태그 display:flex; 두는 것에서 body에 margin: 0 auto 두는 식으로 변경

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,12 @@
 "use client";
 import BottomNavigation from "@/components/Common/BottomNavigation";
+import TopNavigation from "@/components/home/TopNavigation";
 
 const Home = () => {
   return (
     <h1 style={{ color: "white" }}>
       home 화면입니다 히히
+      <TopNavigation />
       <BottomNavigation />
     </h1>
   );
--- src/components/home/TopNavigation.js
+++ src/components/home/TopNavigation.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import NetflixLogo from "public/assets/images/icon/netflix.svg";
+import styled from "styled-components";
 function TopNavigation() {
   return (
-    <div>TopNavigation</div>
-  )
+    <TopNavigationWrapper>
+      <NetflixLogo />
+      <TopMenuItem></TopMenuItem>
+      <TopMenuItem></TopMenuItem>
+      <TopMenuItem></TopMenuItem>
+    </TopNavigationWrapper>
+  );
 }
 
-export default TopNavigation
+const TopNavigationWrapper = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 25.32px;
+`;
+
+const TopMenuItem = styled.li`
+  color: #fff;
+  text-align: center;
+  font-size: 17.197px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30.447px; /* 177.049% */
+  letter-spacing: 0.762px;
+`;
+
+export default TopNavigation;
