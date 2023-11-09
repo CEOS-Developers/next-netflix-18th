c76140a7cc42233a242106d5084916223d8343f7
inyoung <rmdnps10@gmail.com>
2023-11-09 15:02:00 +0900

    Feat: 고정된 하단 내비게이션 메뉴 UI 구현

--- src/components/Common/BottomNavigation.js
+++ src/components/Common/BottomNavigation.js
@@ -1,16 +1,77 @@
 import React from "react";
 import styled from "styled-components";
-import HomeLogo from "public/assets/images/icon/info.svg";
+import HomeLogo from "public/assets/images/icon/home.svg";
+import SearchLogo from "public/assets/images/icon/search.svg";
+import CommingLogo from "public/assets/images/icon/coming-soon.svg";
+import DownloadLogo from "public/assets/images/icon/downloads.svg";
+import MoreLogo from "public/assets/images/icon/more.svg";
+import indicator from "public/assets/images/indicator.png";
+import Image from "next/image";
 function BottomNavigation() {
   return (
-    <BottomNavigationWrapper>
-      <HomeLogo />
-    </BottomNavigationWrapper>
+    <>
+      <BottomNavigationWrapper>
+        <MenuList>
+          <MenuItem padding={"8px"}>
+            <HomeLogo />
+            <MenuText color={"#FFF"}>Home</MenuText>
+          </MenuItem>
+          <MenuItem padding={"8px"}>
+            <SearchLogo />
+            <MenuText>Search</MenuText>
+          </MenuItem>
+          <MenuItem padding={"10px"}>
+            <CommingLogo />
+            <MenuText>Coming Soon</MenuText>
+          </MenuItem>
+          <MenuItem padding={"8px"}>
+            <DownloadLogo />
+            <MenuText>Downloads</MenuText>
+          </MenuItem>
+          <MenuItem padding={"11px"}>
+            <MoreLogo />
+            <MenuText>More</MenuText>
+          </MenuItem>
+        </MenuList>
+        <Image src={indicator} alt={"home-indicator"} width={"100%"} />
+      </BottomNavigationWrapper>
+    </>
   );
 }
 
 const BottomNavigationWrapper = styled.div`
-  width: 100%;
+  position: fixed;
+  bottom: 0px;
+`;
+
+const MenuList = styled.div`
+  display: flex;
+  background: #121212;
+  justify-content: center;
+  gap: 25px;
+  width: 375px;
+  height: 48px;
+`;
+
+const MenuItem = styled.div`
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  padding-top: ${(props) => props.padding};
+  position: relative;
+  cursor: pointer;
+`;
+
+const MenuText = styled.div`
+  position: absolute;
+  bottom: 6px;
+  width: 50px;
+  text-align: center;
+  font-size: 8.2px;
+  font-style: normal;
+  font-weight: 500;
+  color: #8c8787;
+  color: ${(props) => props.color};
 `;
 
 export default BottomNavigation;
