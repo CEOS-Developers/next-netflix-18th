57e2d24539fc374fd66fd1e585850e8edec879f9
inyoung <rmdnps10@gmail.com>
2023-11-09 13:32:19 +0900

    chore: svg 컴포넌트 방식으로 import하기 위한 svgr/webpack 설치

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,5 +1,13 @@
+"use client";
+import BottomNavigation from "@/components/Common/BottomNavigation";
+
 const Home = () => {
-  return <h1 style={{ color: "white" }}>home 화면입니다 히히</h1>;
+  return (
+    <h1 style={{ color: "white" }}>
+      home 화면입니다 히히
+      <BottomNavigation />
+    </h1>
+  );
 };
 
 export default Home;
--- src/components/Common/BottomNavigation.js
+++ src/components/Common/BottomNavigation.js
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import HomeLogo from "public/assets/images/icon/info.svg";
 function BottomNavigation() {
   return (
-    <div>BottomNavigation</div>
-  )
+    <BottomNavigationWrapper>
+      <HomeLogo />
+    </BottomNavigationWrapper>
+  );
 }
 
-export default BottomNavigation
+const BottomNavigationWrapper = styled.div`
+  width: 100%;
+`;
+
+export default BottomNavigation;
