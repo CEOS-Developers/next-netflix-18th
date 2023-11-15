d2d191d82aa328b9424f5b5262346628626e194c
dhsin98 <shinjay417@gmail.com>
2023-11-16 03:14:56 +0900

    feat: 검색 기능

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,8 +5,7 @@ import tmdbApi, { requests } from "@/components/api";
 import NavBar from "@/components/navbar";
 import SearchBar from "./searchBar";
 import { renderMovieLists } from "../utils/movieFunctions";
-import { render } from "react-dom";
-
+import { includesChosung } from "../utils/searchChosung";
 type Props = {};
 interface Movie {
   id: number;
@@ -18,6 +17,16 @@ interface Movie {
 
 function Search({}: Props) {
   const [topRated, setTopRated] = useState<Movie[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredMovies = topRated.filter((movie) =>
+    includesChosung(movie.title, searchQuery),
+  );
+
   useEffect(() => {
     const fetchMovies = async () => {
       try {
@@ -34,14 +43,17 @@ function Search({}: Props) {
 
   return (
     <div className="flex flex-col items-center w-full h-screen">
-      <SearchBar />
+      <SearchBar
+        searchQuery={searchQuery}
+        onSearchChange={handleSearchChange}
+      />
 
       <h3 className="pl-[0.63rem] pt-[117px] text-start text-white text-preview font-bold mb-[1.3rem] w-[375px]">
         Top Searches
       </h3>
 
       <div className="flex flex-col items-center justify-center w-full pb-[60px] overflow-y-auto">
-        {renderMovieLists(topRated)}
+        {renderMovieLists(filteredMovies)}
       </div>
       <NavBar />
     </div>
--- src/app/search/searchBar.tsx
+++ src/app/search/searchBar.tsx
@@ -1,6 +1,11 @@
 import { SearchIcon2, XIcon } from "../../../public/svgs";
 
-const SearchBar = () => {
+interface SearchBarProps {
+  searchQuery: string;
+  onSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBar = ({ searchQuery, onSearchChange }: SearchBarProps) => {
   return (
     <div className="fixed top-[50px] left-1/2 transform -translate-x-1/2 flex items-center bg-[#424242] w-[375px] h-[50px]">
       <div className="absolute inset-y-0 left-0 pl-5 flex items-center pointer-events-none">
@@ -9,10 +14,11 @@ const SearchBar = () => {
 
       <input
         type="text"
+        value={searchQuery}
+        onChange={onSearchChange}
         className="w-full bg-[#424242] text-[#C4C4C4] pl-12 pr-12 focus:outline-none"
         placeholder="Search for a show, movie, genre, e.t.c"
       />
-
       <div className="absolute inset-y-0 right-0 pr-5 flex items-center pointer-events-none">
         <XIcon className="h-5 w-5" />
       </div>
