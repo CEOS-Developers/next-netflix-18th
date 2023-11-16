312ee268b77a5a1c794521b5d915c60b0b938d02
inyoung <rmdnps10@gmail.com>
2023-11-16 15:27:32 +0900

    Feat: 영화상세페이지 스타일링 완료

--- src/components/detail/DetailSection.js
+++ src/components/detail/DetailSection.js
@@ -1,23 +1,27 @@
 import React from "react";
 import Image from "next/image";
+import styles from "./DetailSection.module.css";
+import PlayLogo from "public/assets/images/icon/play.svg";
 function DetailSection({ data, imageUrl }) {
   return (
     <section className="preview">
-      <div className="image-box">
+      <div className={styles.imageBox}>
         <Image
           src={imageUrl + "/" + data.backdrop_path}
           width={375}
           height={415}
+          className={styles.image}
         />
       </div>
-      <div className="button-wrapper">
-        <button className="play-button">
-          <span>Play</span>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.playButton}>
+          <PlayLogo />
+          <span className={styles.playText}>Play</span>
         </button>
       </div>
-      <div className="preview-content">
-        <h1>Previews</h1>
-        <div className="preview-text" style={{ color: "white" }}>
+      <div className={styles.previewContent}>
+        <h1 className={styles.previewTitle}>Previews</h1>
+        <div className={styles.previewText}>
           {data.overview}
         </div>
       </div>
